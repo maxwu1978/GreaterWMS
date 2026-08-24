1a8e9b89e1d15c8f275fffc9f9e44cab27dcb533
Max Wu <wuqxmark@gmail.com>
2026-08-24 16:47:35 -0500

    fix: keep Mail2Task out of warehouse dashboard

--- templates/src/pages/dashboard/dashboard.vue
+++ templates/src/pages/dashboard/dashboard.vue
@@ -1,19 +1,16 @@
 <template>
   <q-page class="dashboard-page flex flex-top column q-pa-sm">
     <operations-board />
-    <mail-task-board />
   </q-page>
 </template>
 
 <script>
 import OperationsBoard from './operationsBoard.vue'
-import MailTaskBoard from './mailTaskBoard.vue'
 
 export default {
   name: 'Pagedashboard',
   components: {
-    OperationsBoard,
-    MailTaskBoard
+    OperationsBoard
   }
 }
 </script>
